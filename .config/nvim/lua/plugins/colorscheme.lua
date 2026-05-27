a47e4d89f71c7dec2fecc03ad5c6f091d632b80b
tom-shimoda <buckeye0204@gmail.com>
2026-05-27 11:22:09 +0900

    ファイルツリー部の背景色変更

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,18 @@ return {
             vim.g.gruvbox_material_transparent_background = 1
             vim.opt.background = "dark"
             vim.cmd("colorscheme gruvbox-material")
+        local function nvimtree_focused()
+            vim.api.nvim_set_hl(0, "NvimTreeNormal",      { bg = "#404040" })
+            vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "#404040" })
+        end
+        local function nvimtree_unfocused()
+            vim.api.nvim_set_hl(0, "NvimTreeNormal",      { bg = "NONE", ctermbg = "NONE" })
+            vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
+        end
+        nvimtree_focused()
+        vim.api.nvim_create_autocmd("ColorScheme",  { callback = nvimtree_focused })
+        vim.api.nvim_create_autocmd("FocusGained",  { callback = nvimtree_focused })
+        vim.api.nvim_create_autocmd("FocusLost",    { callback = nvimtree_unfocused })
         end,
     },
 }
